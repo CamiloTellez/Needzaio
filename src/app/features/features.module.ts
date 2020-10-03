0119222330d8a2ee20c7cce1aeb8f48df296ac20
CamiloT <camilotellezj@gmail.com>
2020-10-03 12:25:10 -0500

    configuracion formulario

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
 import { LoginComponent } from './users/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, FeaturesRoutingModule],
+  imports: [CommonModule, FeaturesRoutingModule, ReactiveFormsModule],
 })
 export class FeaturesModule {}
--- src/app/features/users/login/login.component.ts
+++ src/app/features/users/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  frmLogin: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.frmLogin = this.formBuilder.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.email,
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
+    });
   }
 
+  onSubmit() {}
 }
